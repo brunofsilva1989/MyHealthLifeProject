2a19d341d4c211dbd491076b3d84d29f365a708c
brunofsilva1989 <brunofsilva1989@gmail.com>
2025-03-04 01:20:04 -0300

    Mudanças de Layout

---
+++ MyHealthLife.Web/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyHealthLife.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View("~/Views/Auth/Login.cshtml");
+        }
+    }
+}
